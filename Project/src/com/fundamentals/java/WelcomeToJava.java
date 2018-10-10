571e2570291b25fa4bce587540b5e5d2872b5eb6
tinaimitchell <tinaiwhyso.tm@gmail.com>
2018-10-10 11:17:07 -0500

    This is our first Java project

---
+++ Project/src/com/fundamentals/java/WelcomeToJava.java
@@ -0,0 +1,10 @@
+package com.fundamentals.java;
+
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Java");
+	}
+
+}
