8f11fcf9e46d0939b892fccd334ce40094143467
tinaimitchell <tinaiwhyso.tm@gmail.com>
2018-10-15 12:52:08 -0500

    Went over strings in class example.

--- Project/src/com/fundamentals/java/WelcomeToJava.java
+++ Project/src/com/fundamentals/java/WelcomeToJava.java
@@ -9,10 +9,11 @@ public class WelcomeToJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	someMethod();
-
+	// someMethod();
+	// stringExamples();
+	moreStringExamples();
 	}
-public static void someMethod() {
+	public static void someMethod() {
 //This is a single comment
 /*
  * This
@@ -24,5 +25,44 @@ System.out.println("Welcome to Java");
 }
 
 
-
+	public static void stringExamples() {
+		// String = Type | str1 = variable name | Welcome = string literal
+		String str1 = "Welcome";
+		String str2 = new String("Java");
+		System.out.println(str2);
+		// charAt method returns a single character at a certain position
+		char j = str1.charAt(2);// index is zero based
+		System.out.println(j);
+		// concat method joins two strings together, called concatenation
+		String name = str2.concat(" is cool");
+		System.out.println(name);
+		// equals method will check to see if an object equals the string variable
+		boolean isMyString = str1.equals(str2);  // is str2 = str1?
+		System.out.println(isMyString);
+		// toLowerCase method will make the variable with lower case letters
+		String myString = str1.toLowerCase();
+		System.out.println(myString);
+		// toUpperCase method will make the variable with upper case letters
+		String mySecondString = str2.toUpperCase();
+		System.out.println(mySecondString);
+	}
+	public static void moreStringExamples() {
+		String myString = "My Java String";
+		// Are these equal?
+		boolean isTheSame = myString.toLowerCase().equals("my java string");
+		System.out.println(isTheSame);
+		// myString is capitalized then added to is so capitalized
+		String name = myString.toUpperCase().concat(" is so capitalized");
+		System.out.println(name);
+		// equalsIgnoreCase does not check if it has capitalization. It is ignored
+		boolean testString = myString.equalsIgnoreCase("My java string");
+		System.out.println(testString);
+		// contains method checks to see if it matches part of the variable
+		boolean isContained = myString.contains("ava");
+		System.out.println(isContained);
+		
+		
+		
+	}
+	
 }
